aaf31522c00e04589b25f7d61f56c3d628ef9f9e
Michael Hudson <mnkotagu@gmail.com>
2023-04-22 19:13:15 +0300

    Fix typo in Bubble sort function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,7 +13,7 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
-	if (array == NULL of size < 2)
+	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
